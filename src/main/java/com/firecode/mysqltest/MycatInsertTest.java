e5d0623d35f8158233d5b98551c9fa6f4df85b55
chiangfire <chiang-fire@outlook.com>
2019-07-14 15:23:58 +0800

    fix

--- src/main/java/com/firecode/mysqltest/MycatInsertTest.java
+++ src/main/java/com/firecode/mysqltest/MycatInsertTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 import com.mysql.jdbc.Driver;
 
 /**
- * 测试数据插入
+ * 测试Mycat数据插入
  * 建表语句
  * create table m_user(id int(11) PRIMARY KEY,name char(20) NOT NULL)ENGINE=InnoDB DEFAULT CHARSET=utf8;
  * @author JIANG
@@ -24,18 +24,17 @@ public class MycatInsertTest {
 	 */
 	public static void main(String[] args) throws SQLException, InterruptedException {
 		DriverManager.registerDriver(new Driver());
-		String url = "jdbc:mysql://192.168.229.133:3306/test_test";
-		String username = "admin";
+		String url = "jdbc:mysql://192.168.229.132:8066/test_test";
+		String username = "test_admin";
 		String password = "Jiang@123";
 		Connection connection = DriverManager.getConnection(url,username,password);
-		String sql = "insert into user(name) values(?)";
+		String sql = "insert into m_user(id,name) values(?,?)";
 		PreparedStatement pst = connection.prepareStatement(sql);
-		while(true){
-			for (int i = 0; i < 100; i++) {
-				pst.setString(1, "www"+i);
+			for (int i = 1; i < 10; i++) {
+				pst.setInt(1, i);
+				pst.setString(2, "www"+i);
 				pst.execute();
 			}
 			TimeUnit.SECONDS.sleep(1);
-		}
 	}
 }
